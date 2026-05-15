f3a3bca45534a73b21d776dc77852d46963e176b
mauman <manmau@yahoo.com>
2026-05-15 11:46:50 +0200

    big changes

---
+++ nationalities.py
@@ -0,0 +1,38 @@
+mondo_mappa = {
+    # Europa
+    "albanese": "Albania", "austriaca": "Austria", "austriaco": "Austria",
+    "belga": "Belgio", "bielorussa": "Bielorussia", "bosniaca": "Bosnia Erzegovina",
+    "bulgara": "Bulgaria", "cipriota": "Cipro", "croata": "Croazia",
+    "danese": "Danimarca", "estone": "Estonia", "finlandese": "Finlandia",
+    "francese": "Francia", "greca": "Grecia", "irlandese": "Irlanda",
+    "islandese": "Islanda", "italiana": "Italia", "italiano": "Italia",
+    "lettone": "Lettonia", "lituana": "Lituania", "lussemburghese": "Lussemburgo",
+    "maltese": "Malta", "moldava": "Moldavia", "norvegese": "Norvegia",
+    "olandese": "Paesi Bassi", "polacca": "Polonia", "portoghese": "Portogallo",
+    "britannica": "Regno Unito", "inglese": "Regno Unito", "ceca": "Repubblica Ceca",
+    "rumena": "Romania", "russa": "Russia", "serba": "Serbia", "slovacca": "Slovacchia",
+    "slovena": "Slovenia", "spagnola": "Spagna", "svedese": "Svezia",
+    "svizzera": "Svizzera", "ucraina": "Ucraina", "ungherese": "Ungheria",
+
+    # Africa
+    "algerina": "Algeria", "angolana": "Angola", "camerunense": "Camerun",
+    "egiziana": "Egitto", "egiziano": "Egitto", "etiope": "Etiopia",
+    "ghanese": "Ghana", "keniota": "Kenya", "libica": "Libia",
+    "marocchina": "Marocco", "nigeriana": "Nigeria", "senegalese": "Senegal",
+    "sudafricana": "Sudafrica", "tunisina": "Tunisia",
+
+    # Americhe
+    "argentina": "Argentina", "boliviana": "Bolivia", "brasiliana": "Brasile",
+    "canadese": "Canada", "cilena": "Cile", "colombiana": "Colombia",
+    "cubana": "Cuba", "ecuadoriana": "Ecuador", "messicana": "Messico",
+    "peruviana": "Perù", "statunitense": "Stati Uniti", "americana": "Stati Uniti",
+    "uruguaiana": "Uruguay", "venezuelana": "Venezuela",
+
+    # Asia e Oceania
+    "afghana": "Afghanistan", "australiana": "Australia", "cinese": "Cina",
+    "coreana": "Corea del Sud", "filippina": "Filippine", "giapponese": "Giappone",
+    "indiana": "India", "indonesiana": "Indonesia", "iraniana": "Iran",
+    "irachena": "Iraq", "israeliana": "Israele", "neozelandese": "Nuova Zelanda",
+    "pakistana": "Pakistan", "thailandese": "Thailandia", "turca": "Turchia",
+    "vietnamita": "Vietnam"
+}
